ca827adf964974720811f4115c042b5238ed9581
Deno <denobrockert@yahoo.com>
2019-08-26 08:49:55 -0400

    First push of Capstone 4

---
+++ Capstone4_TaskListWebApp/Controllers/TaskController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Capstone4_TaskListWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone4_TaskListWebApp.Controllers
+{
+    [Authorize]
+    public class TaskController : Controller
+    {
+        private readonly TaskListDbContext _context;
+        public TaskController(TaskListDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ListTasks()
+        {
+            string id = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            List<Tasks> taskList = _context.Tasks.ToList();
+            List<Tasks> userTaskList = new List<Tasks>();
+            foreach(var task in taskList)
+            {
+                if(id == task.UserId)
+                {
+                    userTaskList.Add(task);
+                }
+            }
+
+                
+            return View(userTaskList);
+        }
+
+        public IActionResult AddTask()
+        {
+            
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult AddTask(Tasks newTask)
+        {
+            string id = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            newTask.UserId = id;
+            if (ModelState.IsValid)
+            {
+                _context.Tasks.Add(newTask);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("ListTasks");
+
+        }
+
+        public IActionResult DeleteTask(int Id)
+        {
+            Tasks found = _context.Tasks.Find(Id);
+            if (ModelState.IsValid)
+            {
+                _context.Tasks.Remove(found);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("ListTasks");
+        }
+
+     
+       public IActionResult UpdateTask(int Id)
+       {
+           Tasks found = _context.Tasks.Find(Id);
+           if (ModelState.IsValid && found != null)
+           {
+
+               
+               found.Complete = "Yes";
+
+               _context.Entry(found).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+               _context.Update(found);
+               _context.SaveChanges();
+
+
+
+           }
+           return RedirectToAction("ListTasks");
+           
+
+       }
+
+
+
+    }
+}
---
+++ Capstone4_TaskListWebApp/Models/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capstone4_TaskListWebApp.Models
+{
+    public partial class Tasks
+    {
+        public int TaskId { get; set; }
+        public string UserId { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Complete { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
